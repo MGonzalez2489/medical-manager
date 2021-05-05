3d4e5240bf1401d450c45dfdca735057cce289a6
Manuel <manuel_agh@hotmail.com>
2021-05-05 14:01:24 -0600

    appts form

--- src/app/core/models/db/Appointment.ts
+++ src/app/core/models/db/Appointment.ts
@@ -1,12 +1,16 @@
+import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date-struct';
 import { AppointmentStatus } from '../../enums';
 import { DbModel } from './dbModel';
-import { Doctor } from './Doctor';
-import { Patient } from './Patient';
 
 export class Appointment extends DbModel {
   status: AppointmentStatus;
-  doctor: Doctor;
-  patient: Patient;
+  doctor: string;
+  patient: string;
   startDate: string;
   duration: number; // minutes
+
+  startDateNg?: NgbDateStruct;
+  startTime: string;
+  startTimeNg?: NgbTimeStruct;
 }
--- src/app/shared/components/appointments/appointment-form/appointment-form.component.ts
+++ src/app/shared/components/appointments/appointment-form/appointment-form.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Patient } from 'src/app/core/models/db';
+import { Appointment, Patient } from 'src/app/core/models/db';
 import { faCalendar } from '@fortawesome/free-regular-svg-icons'
-import { Observable } from 'rxjs/internal/Observable';
-import { RequestService } from 'src/app/core/services';
+import { AppService, RequestService, SessionService } from 'src/app/core/services';
 import { BaseSearchCriteria } from 'src/app/core/models/searchCriteria';
-import { map } from 'rxjs/internal/operators/map';
-import { ReturnListModel } from 'src/app/core/models/responses';
+import { KeyNameModel } from 'src/app/core/models';
 
 @Component({
   selector: 'app-appointment-form',
@@ -18,23 +16,53 @@ export class AppointmentFormComponent implements OnInit {
   patient: Patient;
 
   patients: Patient[];
+  appointment = new Appointment();
+  minDate;
   patientSearchCriteira = new BaseSearchCriteria();
   icons = {
     faCalendar
   };
+  meridian = true;
+  constructor(
+    private rService: RequestService,
+    private appService: AppService,
+    private session: SessionService) {
+  }
 
-  constructor(private rService: RequestService) { }
+  initializeForm() {
+    this.appointment.startTimeNg = { hour: 13, minute: 30, second: 0 };
+    const current = new Date();
+    this.minDate = {
+      year: current.getFullYear(),
+      month: current.getMonth() + 1,
+      day: current.getDate()
+    };
+    this.appointment.duration = this.appService.app.appointment.defaultTimeDuration;
+
+    const sessionUser = this.session.getLoggedInUser();
+    this.appointment.doctor = sessionUser.guid;
+
+    if (this.patient) {
+      this.appointment.patient = this.patient.guid;
+    }
+  }
 
   ngOnInit(): void {
-    this.getPatients();
+    const patientName = this.patient ? this.patient.firstName : null;
+    this.getPatients({ term: patientName });
+    this.initializeForm();
   }
-  getPatients(term = null) {
-    if (term) {
-      this.patientSearchCriteira.keyword = term;
+  getPatients($event) {
+    if ($event && $event.term && $event.term.length >= 2) {
+      this.patientSearchCriteira.keyword = $event.term;
       this.rService.getListWithParams<Patient>('patient', this.patientSearchCriteira).subscribe(data => {
         this.patients = data.model;
       });
     }
   }
+  submit() {
+    
+  }
+  cancel() { }
 
 }
